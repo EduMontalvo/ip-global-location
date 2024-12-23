80db0c5c22ba5f72378ea184cfb0d98038eb1fde
Edu Montalvo <kaedu18@gmail.com>
2024-12-23 01:31:03 -0500

    Se culmino el responsive en el indexPage

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -41,28 +41,28 @@ export default function IndexPage() {
         <>
             <AlertMessage Mensaje={AddItemMessage ? 'Se agrego correctamente al historial' : repeatItemMessage ? 'Ya se encuentra en el historial' : ''} open={AddItemMessage || repeatItemMessage} changeIcon={AddItemMessage ? 'success' : repeatItemMessage ? 'warning' : ''} />
 
-            <div className="flex flex-col md:flex-row gap-4 w-4/5 md:w-11/12 md:justify-center items-center md:mt-10 m-auto">
+            <div className="flex flex-col items-center m-auto w-4/5 gap-4 md:flex-row  md:w-11/12 md:justify-center md:h-96 md:mt-10 ">
                 <div
-                    className="bg-contain bg-no-repeat bg-center my-5 rounded-xl h-56 bg-yellow-400 w-full md:w-3/6 md:order-2"
+                    className="bg-contain bg-no-repeat bg-center my-5 rounded-xl h-56 md:h-60 md:w-60 md:rounded-full bg-yellow-400 w-full  md:order-2"
                     style={{ backgroundImage: "url('https://svgsilh.com/svg/2750411.svg')" }}
                 >
                 </div>
-                <div className="text-white flex flex-col gap-4 md:w-3/6 md:order-1 md:px-4">
+                <div className="text-white flex flex-col gap-4 md:w-3/5 md:order-1 md:px-4">
                     <h2 className="text-2xl">Descubre tu ip publica, ubicacion y geolocalizacion gratuita</h2>
                     <p className="text-sm pb-8">Obtener tu ip publica te da acceso a muchas ventajas como la localizacion y la geolocalizacion de tu dispositivo.Ademas tambien podras probar y validar si tu servicio vpn esta activo y funcionando.</p>
-                    <div className="flex justify-around items-center mb-4">
-                        <button className={disableButton ? 'bg-yellow-700 px-4 py-2.5 font-semibold rounded-md' : 'bg-yellow-500 px-4 py-2.5 font-semibold rounded-md'} disabled={disableButton} onClick={handleClickFindMyIp}>Buscar mi ip <ArrowRightOutlined /></button>
-                        <button className='bg-gray-700 border-2 border-yellow-500 text-yellow-500 px-4 py-2 font-semibold rounded-md' onClick={handleClickFindOtherIp}>Buscar otro ip <LoginOutlined /></button>
+                    <div className="flex justify-around items-center mb-4 w-full">
+                        <button className={disableButton ? 'bg-yellow-700 px-4 py-2.5 font-semibold rounded-md md:w-5/12' : 'bg-yellow-500 px-4 py-2.5 font-semibold rounded-md md:w-5/12'} disabled={disableButton} onClick={handleClickFindMyIp}>Buscar mi ip <ArrowRightOutlined /></button>
+                        <button className='bg-gray-700 border-2 border-yellow-500 text-yellow-500 px-4 py-2 font-semibold rounded-md md:w-5/12' onClick={handleClickFindOtherIp}>Buscar otro ip <LoginOutlined /></button>
                     </div>
                 </div>
-
+                {/* md:w-5/12 */}
             </div>
             {stateInput ? (<FormSearchIp />) : (<></>)}
             {visibility ?
                 (<>
-                    <div className="grid ">
+                    <div className="grid grid-cols-1 py-10 md:grid-cols-2 md:grid-rows-3 md:gap-y-10 w-11/12 mx-auto ">
                         <div
-                            className="bg-cover bg-center w-4/5 m-auto mt-10 flex flex-col items-center justify-center gap-4  rounded-xl h-96"
+                            className="bg-cover bg-center w-4/5 m-auto mt-10 flex flex-col items-center justify-center gap-4  rounded-xl h-96 order-1"
                             style={{ backgroundImage: "url('https://images.pexels.com/photos/593158/pexels-photo-593158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')" }}
                         >
                             <div className="flex flex-col justify-center opacity-50 bg-black border-2 border-white  rounded-xl p-4 h-4/6 w-11/12">
@@ -86,7 +86,7 @@ export default function IndexPage() {
                             </div>
                         </div>
                         <div
-                            className="bg-cover bg-center  w-4/5 m-auto mt-10 flex flex-col items-center justify-center gap-4  rounded-xl h-96 "
+                            className="bg-cover bg-center md:row-span-1 md:col-span-2 md:w-5/12 md:m-auto w-4/5 m-auto mt-10 flex flex-col items-center justify-center gap-4  rounded-xl h-96 order-3"
                             style={{ backgroundImage: "url('https://images.pexels.com/photos/593159/pexels-photo-593159.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')" }}
                         >
                             <div className="flex flex-col justify-center opacity-80 bg-black border-2 border-white  rounded-xl p-4 h-4/6 w-11/12">
@@ -110,7 +110,7 @@ export default function IndexPage() {
                             </div>
                         </div>
                         <div
-                            className="bg-cover bg-center w-4/5 m-auto mt-10 flex flex-col items-center justify-center gap-4  rounded-xl h-96"
+                            className="bg-cover bg-center w-4/5 m-auto mt-10 flex flex-col items-center justify-center gap-4  rounded-xl h-96 order-2"
                             style={{ backgroundImage: "url('https://images.pexels.com/photos/29149883/pexels-photo-29149883/free-photo-of-detalle-de-arquitectura-geometrica-moderna-abstracta.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')" }}
                         >
                             <div className="flex flex-col justify-center opacity-80 bg-black border-2 border-white  rounded-xl p-4 h-4/6 w-11/12">
@@ -132,7 +132,7 @@ export default function IndexPage() {
                                 </div>
                             </div>
                         </div>
-                        <div className="w-4/5 m-auto mt-10">
+                        <div className="w-4/5 m-auto mt-10 md:row-span-1 md:col-span-2 md:w-11/12 order-4">
                             <div className="h-96 w-full z-0 relative">
                                 <Map latitude={resultIpData.latitude} longitude={resultIpData.longitude} />
                             </div>
@@ -140,7 +140,7 @@ export default function IndexPage() {
                     </div>
                     <div className="flex flex-col justify-center items-center gap-4 mt-8">
                         <h3 className="text-white font-extralight"> Guardar en tu historial de busqueda?  </h3>
-                        <div className="w-4/5 m-auto mt-5">
+                        <div className="w-4/5 m-auto mt-5 md:w-2/12">
                             <button className="bg-yellow-500 hover:bg-yellow-700 font-bold py-2 px-4 rounded w-full" onClick={() => addToHistory(resultIpData)}>Guardar</button>
                         </div>
                     </div>
